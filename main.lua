e09361867f1af4b851848eeb24cfd00cf032e3ca
bradleymoore111 <bradleym007@gmail.com>
2020-10-04 02:24:26 -0700

    Removed useless debug code

--- main.lua
+++ main.lua
@@ -1,23 +1,12 @@
 local LostNoD4	 = RegisterMod("Lost No D4", 1)
 
-function LostNoD4:Debug(message)
-    if message == nil then
-        message = "received nil"
-    end
-
-    local msg = "[Lost Hope]: " .. message
-	-- Isaac.DebugString(msg)
-end
-
 -- returns true if the player is The Lost
 function LostNoD4:IsTheLost()
     local player = Isaac.GetPlayer(0)
     if player:GetPlayerType() == PlayerType.PLAYER_THELOST then
-        --LostNoD4:Debug("I'm The Lost")
         return true
     end
 
-    --	LostNoD4:Debug("I'm not The Lost")
     return false
 end
 
